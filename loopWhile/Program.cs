c8f54f68df95e02bbfdac915614c275c64ce024d
Mohamed M Sabry <113486193+msabry2022@users.noreply.github.com>
2023-06-03 18:03:30 +0300

    Add files via upload

---
+++ loopWhile/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loopWhile
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //int x = 1;
+            //while (x <= 10) 
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //}
+
+            //int x = 0;
+            //do
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //} while (false);
+
+
+            // calculator of 2 numbers with 
+            // (+ - * / )
+            // -----------------------------
+
+            // variable to make many calculations
+            string strYN;
+
+            do
+            {
+                // other variables
+                int num1;
+                int num2;
+                string cal;
+                int result;
+
+                // aske user to enter 2 numbers
+                Console.Write("enter number 1 : ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.Write("enter number 2 : ");
+                num2 = int.Parse(Console.ReadLine());
+
+                // ask user to enter calculation
+                // (+ - * /)
+                Console.Write("enter calculation (+ or - or * or / : ");
+                cal = Console.ReadLine();
+                    if (cal == "+")
+                    {
+                        result = Convert.ToInt32(num1+num2);
+                        Console.WriteLine(result);
+                    }
+                    else if(cal == "-")
+                    {
+                        result= Convert.ToInt32(num2-num1);
+                        Console.WriteLine(result);
+                    }
+                    else if(cal == "*")
+                    {
+                        result = Convert.ToInt32(num1*num2);
+                        Console.WriteLine(result);
+                }
+                    else if(cal == "/")
+                    {
+                        if (num1 > num2)
+                        {
+                        result = Convert.ToInt32(num1 / num2);
+                        }
+                        else
+                        {
+                        result = Convert.ToInt32(num2 / num1);
+                    }
+                    Console.WriteLine(result);
+                }
+                    else
+                    {
+                    Console.WriteLine("not a valid value, try again");
+                    }
+
+                // ask user to make another calculation
+                Console.WriteLine("****************");
+                Console.Write("do you want another calculation (y/n) : " );
+                strYN = Console.ReadLine();
+            }   while (strYN == "y" || strYN == "Y");
+        }
+    }
+}
